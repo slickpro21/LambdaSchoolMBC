91bcb7fd76c3cf247e51edcd89c27e9cfc0ef0d1
Tyler Moyer <33009040+slickpro21@users.noreply.github.com>
2020-03-14 13:48:56 -0400

    New Personal Webpage
    Lambda Pre Course Launch a Website Exercise.

---
+++ Lambda.web.page/index.js
@@ -0,0 +1,9 @@
+let button = document.getElementById("emphasize-insta");
+
+button.onmouseover = function () {
+    this.style.color = "#FFFFFF";
+}
+
+button.onmouseout = function () {
+    this.style.color = "#373633";
+}
